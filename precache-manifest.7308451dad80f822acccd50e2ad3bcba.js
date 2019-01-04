d9b1712e3e1df543458fbee632973930e0231afb
pomyo <yeddev@gmail.com>
2019-01-03 21:42:49 -0500

    Updates

---
+++ precache-manifest.7308451dad80f822acccd50e2ad3bcba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7701815b74baf89f84",
+    "url": "/recipeapp/static/js/runtime~main.af770181.js"
+  },
+  {
+    "revision": "c80e16732d4a621521bc",
+    "url": "/recipeapp/static/js/main.c80e1673.chunk.js"
+  },
+  {
+    "revision": "3912e494b4cc3a5d3eda",
+    "url": "/recipeapp/static/js/1.3912e494.chunk.js"
+  },
+  {
+    "revision": "c80e16732d4a621521bc",
+    "url": "/recipeapp/static/css/main.da862f87.chunk.css"
+  },
+  {
+    "revision": "b009403cb75bd0a9dee554bc17fbfef9",
+    "url": "/recipeapp/index.html"
+  }
+];
